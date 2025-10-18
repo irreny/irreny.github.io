38582e1f79b1bfc49ea767c6c53c14b80d078608
Irina Babkina <i.p.ponomareva@yandex.ru>
2025-10-18 22:26:54 +0300

    Update js.js

--- js.js
+++ js.js
@@ -3,7 +3,7 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Главное не всё знать, а загуглить на английском')
+	alert('lets keep going :) ')
 })
 
 const stack = document.querySelector('.stack');
